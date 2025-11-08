30439c297c54e0e0853d6fb1fa04f5db17d86aca
zxd1385 <zxd1385zxd1385@gmail.com>
2025-11-08 21:23:55 +0330

    3_2 part is ready to test practicaly

---
+++ FirstSession_MousapourHDLs/user/src/3_2/And_Xor_circuit.v
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+
+module And_Xor(
+    input wire [3:0] button,
+    output wire [4:0] LED
+);
+
+wire g;
+wire f;
+
+assign g = button[2] & button[3];
+assign f = button[0] & button[1];
+
+assign LED[0] = g;
+assign LED[1] = f ^ g;
+
+endmodule
